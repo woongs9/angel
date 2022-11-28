616a9accf0dd34c98dfbb169ee4384aabc8bb80e
choi-wonseok <apples226@hotmail.com>
2022-11-28 12:08:16 +0900

    2

--- src/main/java/com/toilet/urgent/controller/MainController.java
+++ src/main/java/com/toilet/urgent/controller/MainController.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.tags.Param;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 
@@ -16,9 +20,20 @@ import java.util.List;
 public class MainController {
     private final ToiletService toiletService;
     @GetMapping({"/main"})
-    public String main(Toilet toilet, Model model){
+    public String main(Toilet toilet, Model model,  @RequestParam(required = false) String lat,
+                       @RequestParam(required = false) String lon){
         List<Toilet> toiletList = toiletService.listToilet(toilet);
+        System.out.println(lon);
+        System.out.println(lat);
+        System.out.println("---");
         model.addAttribute("list", toiletList);
         return "main";
+    }
+
+    @GetMapping({"/test"})
+    public String test(Toilet toilet, Model model){
+        List<Toilet> toiletList = toiletService.listToilet(toilet);
+        model.addAttribute("list", toiletList);
+        return "test";
     }
 }
